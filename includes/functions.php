f2685dd8596e01eaad86d56c06304ed6ee1b8114
Helight.Xu <Helight.Xu@49a521ce-3dcc-11de-b5fc-91815ed80701>
2009-05-11 01:41:23 +0000

    version 0.01

---
+++ includes/functions.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+function getFeed($feed_url) {
+	
+	$content = file_get_contents($feed_url);
+	echo $content;
+	$x = new SimpleXmlElement($content);
+	
+	echo "<ul id='test'>";
+	
+	foreach($x->channel->item as $entry) {
+		echo "<li><h2>". $entry->title ."</h2>";
+		echo "<div style='display:none'>".$entry->description."</div></li> ";
+		}
+	echo "</ul>";
+}
+?>
