eee253fa67cbe670b352094e3b10bc5245c0e6e7
Helight.Xu <Helight.Xu@49a521ce-3dcc-11de-b5fc-91815ed80701>
2010-01-01 10:49:43 +0000

    new version

--- includes/functions.php
+++ includes/functions.php
@@ -14,4 +14,26 @@ function getFeed($feed_url) {
 		}
 	echo "</ul>";
 }
+
+function show_list()
+{
+	if ($rs = $rss->get("../xy_members.xml")) {
+		//echo $rs[description]."<br><br>\n";
+		echo "<div style='height: 560px;' id='subTree' class='scrolled accordion-body'>";
+		foreach ($rs['items'] as $item) {
+		
+		echo "<ul class='subscribes'><li><a class='all-readed' title='".$item[title]."' href='#.$item[link].' >";
+		echo "<strong><span class='title'>".$item[title]."</span></strong></a>";
+		echo "</li></ul>";
+		}
+		if ($rs['items_count'] <= 0) {
+			echo "<li>Sorry, no items found in the RSS file :-(</li>"; 
+		}
+		echo "</div>\n";
+	}
+	else {
+		echo "Sorry: It's not possible to reach RSS file $url\n<br />";
+		// you will probably hide this message in a live version
+	}
+}
 ?>
