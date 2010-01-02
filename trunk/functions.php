f95a39c9de317cae1b25265bc3592634a32eb88f
Helight.Xu <Helight.Xu@49a521ce-3dcc-11de-b5fc-91815ed80701>
2010-01-02 03:28:40 +0000

    version 0.03

--- trunk/functions.php
+++ trunk/functions.php
@@ -16,65 +16,94 @@ function getFeed($feed_url) {
 	echo "</ul>";
 }
 
-function show_list($rss)
-{
-?>
-	<div id="feedFolderHeader" class="fixed accordion-header expanded">
-		<span style="cursor: default;" class="sprite">-</span>
-		<a style="cursor: default;" href="#" title="04级及以往">
-		<strong style="cursor: default;">
-		<span style="cursor: default;" id="subSpan">06级及以往</span></strong></a>
-	</div>
-<?php
-	if ($rs = $rss->get("./xy_members.xml")) {
-	echo "<div style='height: 560px;' id='subTree' class='scrolled accordion-body'>";
-	foreach ($rs['items'] as $item) {
-		echo "<ul class='subscribes'><li><a class='all-readed' title='".$item[title]."' href='?feed_url=".$item[link]."' >";
-		echo "<strong><span class='title'>".$item[title]."</span></strong></a>";
-		echo "</li></ul>";
-	}
-	if ($rs['items_count'] <= 0) {
-		echo "<li>Sorry, no items found in the RSS file :-(</li>"; 
-	}
-	echo "</div>\n";
-	} else {
-		echo "Sorry: It's not possible to reach RSS file $url\n<br />";
-	}
-?>
-	<div id="feedFolderHeader" class="fixed accordion-header expanded">
-		<span style="cursor: default;" class="sprite">-</span>
-		<a style="cursor: default;" href="#" title="05级成员">
-		<strong style="cursor: default;">
-		<span style="cursor: default;" id="subSpan">05级成员</span></strong></a>
-	</div>
-	<div style="height: 560px;" id="subTree" class="scrolled accordion-body">
-	  <ul class="subscribes">
-		<li>
-		<a class="all-readed" title="A Geek's Page" href="#" >
-		<strong><span class="title">dddddd</span></strong></a>
-		</li>
-	  </ul>
-	</div>
-	<div id="feedFolderHeader" class="fixed accordion-header expanded">
-		<span style="cursor: default;" class="sprite">-</span>
-		<a style="cursor: default;" href="#" title="05级成员">
-		<strong style="cursor: default;">
-		<span style="cursor: default;" id="subSpan">04级成员</span></strong></a>
-	</div>
-	<div style="height: 560px;" id="subTree" class="scrolled accordion-body">
-	  <ul class="subscribes">
-		<li>
-		<a class="all-readed" title="A Geek's Page" href="#" >
-		<strong><span class="title">A Geek's Page</span></strong></a>
-		</li>
-	  </ul>
-	</div>
-<?php
-}
-
 function show_top()
 {
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>西邮RSS阅读</title>
+	<link rel="stylesheet" type="text/css" href="css/reader.css">
+	<script type="text/javascript" src="js/jquery-1.3.2.js"></script>
+	<script type="text/javascript" src="js/ui.core.js"></script>
+	<script type="text/javascript" src="js/ui.accordion.js"></script>
+	<script type="text/javascript" src="js/ddaccordion.js"></script>
+</head>
+<script type="text/javascript">
+ddaccordion.init({
+	headerclass: "expandable", 
+	contentclass: "categoryitems", 
+	revealtype: "click", 
+	mouseoverdelay: 200, 
+	collapseprev: true, 
+	defaultexpanded: [0],
+	onemustopen: false, 
+	animatedefault: false, 
+	persiststate: true, 
+	toggleclass: ["", "openheader"], 
+	togglehtml: ["prefix", "", ""], 
+	animatespeed: "fast",
+	oninit:function(headers, expandedindices){ 
+		//do nothing
+	},
+	onopenclose:function(header, index, state, isuseractivated){ 
+		//do nothing
+	}
+})
+</script>
+<style type="text/css">
+.arrowlistmenu{
+width: 280px; /*width of accordion menu*/
+}
+
+.arrowlistmenu .menuheader{ /*CSS class for menu headers in general (expanding or not!)*/
+font: bold 14px Arial;
+color: black;
+background: #aaa url(titlebar.png) repeat-x center left;
+margin-bottom: 10px; /*bottom spacing between header and rest of content*/
+text-transform: uppercase;
+padding: 4px 0 4px 10px; /*header text is indented 10px*/
+cursor: hand;
+cursor: pointer;
+}
+
+.arrowlistmenu .openheader{ /*CSS class to apply to expandable header when it's expanded*/
+background-image: url(titlebar-active.png);
+}
+
+.arrowlistmenu ul{ /*CSS for UL of each sub menu*/
+list-style-type: none;
+margin: 0;
+padding: 0;
+margin-bottom: 8px; /*bottom spacing between each UL and rest of content*/
+}
+
+.arrowlistmenu ul li{
+padding-bottom: 2px; /*bottom spacing between menu items*/
+}
+
+.arrowlistmenu ul li a{
+color: #A70303;
+background: url(arrowbullet.png) no-repeat center left; /*custom bullet list image*/
+display: block;
+padding: 2px 0;
+padding-left: 19px; /*link text is indented 19px*/
+text-decoration: none;
+font-weight: bold;
+border-bottom: 1px solid #dadada;
+font-size: 90%;
+}
+
+.arrowlistmenu ul li a:visited{
+color: #A70303;
+}
+
+.arrowlistmenu ul li a:hover{ /*hover state CSS*/
+color: #A70303;
+background-color: #F3F3F3;
+}
+</style>
+
 <body class="mozilla">
 <div id="top">
   <div id="topBgLeft"></div><div id="topBgRight"></div>
@@ -99,6 +128,116 @@ function show_top()
   <div id="topSearch">
  Logo 标志
   </div>
+</div>
+<?php
+}
+
+function show_list($rss)
+{
+?>
+	<div id="feedFolderHeader" class="fixed accordion-header expanded">
+		<span style="cursor: default;" class="sprite">-</span>
+		<a style="cursor: default;" href="#" title="04级及以往">
+		<strong style="cursor: default;">
+		<span style="cursor: default;" id="subSpan">06级及以往</span></strong></a>
+	</div>
+<?php
+	if ($rs = $rss->get("./xy_members.xml")) {
+	echo "<div class='scrolled accordion-body'>";
+	echo "<ul class='subscribes'>";
+	foreach ($rs['items'] as $item) {
+		echo "<li><a class='all-readed' title='".$item[title]."' href='?feed_url=".$item[link]."'>";
+		echo "<strong><span class='title'>".$item[title]."";
+		echo "</span></strong></a></li>";
+	}
+	if ($rs['items_count'] <= 0) {
+		echo "<li>Sorry, no items found in the RSS file :-(</li>"; 
+	}
+	echo "</ul></div>\n";
+	} else {
+		echo "Sorry: It's not possible to reach RSS file $url\n<br />";
+	}
+?>
+	<div id="feedFolderHeader" class="fixed accordion-header expanded">
+		<span style="cursor: default;" class="sprite">-</span>
+		<a style="cursor: default;" href="#" title="05级成员">
+		<strong style="cursor: default;">
+		<span style="cursor: default;" id="subSpan">05级成员</span></strong></a>
+	</div>
+	<div style="height: 560px;" id="subTree" class="scrolled accordion-body">
+	  <ul class="subscribes">
+		<li>
+		<a class="all-readed" title="A Geek's Page" href="#" >
+		<strong><span class="title">dddddd</span></strong></a>
+		</li>
+	  </ul>
+	</div>
+<?php
+}
+
+function show_list2($rss)
+{
+?>
+<div class="arrowlistmenu">
+
+<h3 class="menuheader expandable">06级成员</h3>
+<ul class="categoryitems">
+<?php
+	if ($rs = $rss->get("./xy_members.xml")) {
+
+	foreach ($rs['items'] as $item) {
+		echo "<li><a title='".$item[title]."' href='?feed_url=".$item[link]."'>";
+		echo "<strong>".$item[title]."</strong></a></li>";
+	}
+	if ($rs['items_count'] <= 0) {
+		echo "<li>Sorry, no items found in the RSS file :-(</li>"; 
+	}
+
+	} else {
+		echo "<li>Sorry: It's not possible to reach RSS file $url\n</li>";
+	}
+?>
+</ul>
+<h3 class="menuheader expandable">05级成员</h3>
+<ul class="categoryitems">
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+</ul>
+
+<h3 class="menuheader expandable">04级及以往</h3>
+<ul class="categoryitems">
+<li><a href="#" >测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+</ul>
+
+<h3 class="menuheader expandable">04级及以往</h3>
+<ul class="categoryitems">
+<li><a href="#" >测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+<li><a href="#">测试标签</a></li>
+</ul>
 </div>
 <?php
 }
