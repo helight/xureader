e2f93ca375f993cc7e19bad5f0e7e19f0c81d3fe
Helight.Xu <Helight.Xu@49a521ce-3dcc-11de-b5fc-91815ed80701>
2010-02-05 08:29:38 +0000

    new version

--- trunk/fun.php
+++ trunk/fun.php
@@ -1,32 +1,38 @@
 ﻿<?php
-include "./includes/lastRSS.php"; 
-
-function getFeed($feed_url) {
-	
-	$content = file_get_contents($feed_url);
-	echo $content;
-	$x = new SimpleXmlElement($content);
-	
-	echo "<ul id='test'>";
-	
-	foreach($x->channel->item as $entry) {
-		echo "<li><h2>". $entry->title ."</h2>";
-		echo "<div style='display:none'>".$entry->description."</div></li> ";
-		}
-	echo "</ul>";
-}
+	include "./includes/lastRSS.php"; 
+	$rss = new lastRSS;
+	$rss->cache_dir = './temp';
+	$rss->cache_time = 1200; 
+	$rss2 = new lastRSS;
+	$rss2->cache_dir = './temp';
+	$rss2->cache_time = 1200; 
 
 function show_top()
 {
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>西邮RSS阅读</title>
 	<link rel="stylesheet" type="text/css" href="css/css.css">
-	<link rel="stylesheet" type="text/css" href="css/reader.css">
 	<script type="text/javascript" src="js/jquery-1.2.1.js"></script>
 	<script type="text/javascript" src="js/ddaccordion.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/toggleElements.css" />
+	<script type="text/javascript" src="js/jquery.toggleElements.pack.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('div.toggler1').toggleElements( );
+	});
+	
+function doOnClick() {
+	alert('callback: onClick');
+}
+function doOnHide() {
+	alert('callback: onHide');
+}
+function doOnShow() {
+	alert('callback: onShow');
+}
+</script>
 </head>
 <script type="text/javascript">
 ddaccordion.init({
@@ -50,38 +56,30 @@ ddaccordion.init({
 	}
 })
 </script>
-
-<body class="mozilla">
+<body >
 <div id="top">
-  <div id="topBgLeft"></div><div id="topBgRight"></div>
-  <ul id="topNav">
-    <li class="tn-my-subscribes">
    	<a id="readTab" href="#" title="我的阅读" class="current">我的阅读</a>
-    </li>
-  </ul>
-
-  <div id="topSubNav">
-    <ul class="fl">
-      <li><a href="http://www.xiyoulinux.cn/" target="_blank">小组首页</a></li>
-      <li class="slice">|</li>
-      <li><a href="#" target="_blank">小组博客</a></li>
-      <li class="slice">|</li>
-      <li><a id="showMoreItemsMenu" href="#" class="popmenu">更多</a></li>
-    </ul>
-    <ul class="fr">
-	<li>Helight.Xu@Gmail.com</a></li>
-    </ul>
-  </div>
-  <div id="topSearch">
- Logo 标志
-  </div>
 </div>
 <?php
 }
+?>
 
-function show_list($rss)
+<?php
+function show_bottom()
 {
-
+?>
+<div>
+note!!!
+</div>   
+</body></html>
+<?php
+}
+?>
+<!-- function show_list() -->
+<?php
+function show_list()
+{
+	global $rss;
 	echo "<div class=\"arrowlistmenu\">";
 	if ($rs = $rss->get("./xy_members.xml")) {
 
@@ -110,8 +108,7 @@ function show_list($rss)
 			$category4title[$c4] = $item[title];
 			$category4link[$c4] = $item[link];
 			$c4++; 
-		}
-		
+		}		
 	}
 
 	if ($rs['items_count'] <= 0) {
@@ -153,46 +150,43 @@ function show_list($rss)
 		echo "<strong>".$category4title[$i]."</strong></a></li>";
 	}
 	echo "</ul>";
-?>	
-
-</div>
-<?php
-}
-
-function show_bottom()
-{
-?>
-
-<div class="fixed" id="contentFunctionBar">
-
-  <div class="fl">
-    <div id="contentListType">
-      <span class="label">显示：</span>
-      <span class="selection current foolinkL" style="margin-right: 0pt;">
-      <span class="foolinkR">显示全部</span>
-      </span>
-    </div>
-    <span><a href="#">分享此订阅</a></span>
-  </div>
-
-  <div id="contetDisplayType" class="fr">
-    <span class="selection foolinkL current" style="margin-right: 0pt;">
-      <span class="foolinkR">标题模式</span>
-    </span><span class="label">-</span>
-  </div>
-<!-- xxxxxxxxxxxxxxxxx -->
-</div>
-</ul>
-	<div class="fixed" id="contentPageBar">notes</div>
-</div>
-</div>
-</div>
-    
-    <div id="chcoll-main" class="_full_page_ hidden"></div>
-    <div id="hotArticles" class="_full_page_ hidden"></div>
-    <div id="welcomePage" class="_full_page_ hidden" style="margin-top: 65px;"></div>
-   
-</body></html>
-<?php
+	echo "</div>";//<!-- end of list show -->
 }
 ?>
+<!-- function ShowOneRSS($url) -->
+<?php
+function ShowOneRSS($url) {
+	global $rss2;
+
+	echo "<div class=\"contentWrapper\">";
+
+	if (($rs = $rss2->get($url)) && ($url != '')) {
+	echo "<div >";
+	echo "<span>";
+	echo "<a target='_blank' href='".$rs[link]."' id='titleSourceLink'>".$rs[title]."</a></span>";
+	echo "<span>".$rs[description]."</span>";
+	echo "</div>";
+
+	echo "<div><br>";	//<!-- begin show rss-->
+
+	foreach ($rs['items'] as $item) {
+	//<!-- one item -->	
+	echo "<div class=\"toggler1\" title=".$item[title].">";
+	echo "<div class=\"rsscontent\">";
+	echo "<div class='rtitle'><a target='_blank' title='到原网页查看全文' ";
+	echo "href='".$item[link]."'>".$item[title]."<img src='img/icon_go.gif'></a>";
+	echo "<span class='stat'> 作者：helight 发表时间：Time</span></div>";
+	echo "<div><br>";		
+		$out = strtr($item[description], array('<![CDATA['=>'', ']]>'=>''));
+		echo "<div>".$out."</div>";
+	echo "</div><br>";
+	echo "<div class=\"addrssto\">加入收藏其他</div>";
+	echo "</div>";
+	echo "</div>";		//<!-- end one item -->
+	}
+	echo "</div>";		//<!-- end show rss-->
+	}
+	echo "</div>";		//<!-- end of contentWrapper -->
+}
+?>
+<!-- end of function -->
